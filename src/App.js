76febbe1f3dbc20fdffc563ef46da4e28e04ffe7
suryadoulagar <suryadoulagar@gmail.com>
2021-01-31 01:43:06 +0530

    chat params added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Nexus build</h1>
       {/* Header */}
-      <Header />
+      <Router>
+        <Header />
 
-      <div className="app__body">
-        {/* Sidebar */}
-        <Sidebar />
-        {/* React-router */}
-      </div>
+        <div className="app__body">
+          {/* Sidebar */}
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+          {/* React-router */}
+        </div>
+      </Router>
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
-import InsertCommentIcon from '@material-ui/icons/InsertComment';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import AppsIcon from '@material-ui/icons/Apps';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import AddIcon from '@material-ui/icons/Add';
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
 import SidebarOption from "./SidebarOption";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import db from "./firebase";
 
 function Sidebar() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    //runs when sidebar component loads
+    db.collection("rooms").onSnapshot((Snapshot) =>
+      setChannels(
+        Snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+        }))
+      )
+    );
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -39,6 +54,14 @@ function Sidebar() {
       <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
       <hr />
       <SidebarOption Icon={AddIcon} title=" Add Channel" />
+
+      {/* connect to database and list all channels*/}
+      {/* <sidebarOPtion /> */}
+
+      {channels.map(channel => (
+        <SidebarOption title={channel.name} id={channel.id} />
+      ))}
+
     </div>
   );
 }
