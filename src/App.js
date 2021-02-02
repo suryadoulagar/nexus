26c60a6d3be504afe0d93d1d4780025ef43dea9e
suryadoulagar <suryadoulagar@gmail.com>
2021-02-02 18:55:51 +0530

    added chat ui

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       {/* Header */}
       <Router>
         <Header />
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./SidebarOption.css";
 
-function SidebarOption({ Icon, title }) {
+function SidebarOption({ Icon, title, id, addChannelOption }) {
   return (
-    <div className="sidebarOption">
+    <div className="sidebarOption" onClick={addChannelOption ? addChannel : selectChannel}>
       {Icon && <Icon className="sidebarOption__icon" />}
       {Icon ? (
         <h3>{title}</h3>
       ) : (
         <h3 className="sidebarOption__room">
-          <span className="sidebarOption__hash"># {title}</span>
+          <span className="sidebarOption__hash">#</span>{title}
         </h3>
       )}
     </div>
