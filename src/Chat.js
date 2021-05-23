33ab3247d45a7c5c1e774b571b7faae600f367e2
suryadoulagar <suryadoulagar@gmail.com>
2021-05-23 06:40:06 +0530

    fetching Messages from DB

--- src/Chat.js
+++ src/Chat.js
@@ -1,28 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./Chat.css";
-import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined'
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import Message from "./Message";
+import db from "./firebase";
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+  const [roomMessages, setRoomMessages] = useState([]);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
+    }
+
+    db.collection("rooms")
+      .doc(roomId)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) =>
+        setRoomMessages(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, [roomId]);
+
+  console.log(roomDetails);
+  console.log(roomMessages);
 
   return (
     <div className="chat">
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
-            <strong>#general</strong>
+            <strong>#{roomDetails?.name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
         <div className="chat__headerRight">
-        <p>
-          <InfoOutlinedIcon /> Details
-        </p>
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
         </div>
-        
       </div>
+
+      <div className="chat__messages">
+        {roomMessages.map(({message, timestamp, user, userImage}) =>(
+          <Message
+          message = {message}
+          timestamp = {timestamp}
+          user = {user}
+          userImage = {userImage}
+          />
+        ))}
+        </div>
     </div>
   );
 }
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -4,12 +4,11 @@ import "./SidebarOption.css";
 import db from "./firebase";
 
 function SidebarOption({ Icon, title, id, addChannelOption }) {
-
   const history = useHistory();
 
   const SelectChannel = () => {
-    if(id) {
-      history.push(`/room/${id}`)
+    if (id) {
+      history.push(`/room/${id}`);
     } else {
       history.push(title);
     }
@@ -18,21 +17,25 @@ function SidebarOption({ Icon, title, id, addChannelOption }) {
   const addChannel = () => {
     const channelName = prompt("please enter channel name.");
 
-    if(channelName) {
-      db.collection('rooms').add({
-        name : channelName,
-      })
+    if (channelName) {
+      db.collection("rooms").add({
+        name: channelName,
+      });
     }
   };
 
   return (
-    <div className="sidebarOption" onClick={addChannelOption ? addChannel : SelectChannel} >
+    <div
+      className="sidebarOption"
+      onClick={addChannelOption ? addChannel : SelectChannel}
+    >
       {Icon && <Icon className="sidebarOption__icon" />}
       {Icon ? (
         <h3>{title}</h3>
       ) : (
         <h3 className="sidebarOption__room">
-          <span className="sidebarOption__hash">#</span>{title}
+          <span className="sidebarOption__hash">#</span>
+          {title}
         </h3>
       )}
     </div>
